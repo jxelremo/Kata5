aa64427890ee5f82eb7bbe68b8553930451a4689
usuario <usuario@10.22.143.36>
2015-11-11 10:03:51 +0000

    Second commit

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -1,5 +1,10 @@
 package kata5;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,7 +13,7 @@ import java.sql.Statement;
 
 public class Kata5 {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
         Class.forName("org.sqlite.JDBC");
         Connection conec = DriverManager.getConnection("jdbc:sqlite:DDBBKATA");
         Statement state = conec.createStatement();
@@ -18,5 +23,16 @@ public class Kata5 {
             System.out.println("ID = "+rs.getInt("ID"));
             System.out.println("NAME = "+rs.getString("NAME"));
         }
+        
+        String nameFile ="C:\\Users\\usuario\\Documents\\NetBeansProjects\\Kata5\\emailsfilev1.txt";
+        BufferedReader reader = new BufferedReader (new FileReader(new File(nameFile)));
+        String mail;
+        while((mail=reader.readLine())!=null){
+            String query ="INSERT INTO MAIL (MAIL) VALUES ('"+mail+"')";
+            state.executeUpdate(query);
+        }
+        rs.close();
+        state.close();
+        conec.close();
     }
 }
